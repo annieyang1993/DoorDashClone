0a8289a3ec3c5a7d2868be3faf7c7e626b10787a
Annie Yang <annieqiyang@gmail.com>
2021-10-12 13:52:36 -0400

    stuff

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ end
 
 
 
-ruby '2.6.0'
+ruby '2.7.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "DoorDash_Clone_Final_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "door_dash_clone_final_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
